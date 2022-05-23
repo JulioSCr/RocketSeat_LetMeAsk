fa1cb2f4e1dcc4a55c82e5a0391e8c5ca8c5853a
Julio Cruz <julio.cruz@7comm.com.br>
2022-05-22 22:54:14 -0300

    Room

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
+import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
 import { useAuth } from '../hooks/useAuth';
 import { database } from '../services/firebase';
@@ -24,7 +25,7 @@ type FirebaseQuestions = Record<string, {
     isHighlighted: boolean;
 }>
 
-type Question = {
+type QuestionType = {
     id: string;
     author: {
         name: string;
@@ -40,7 +41,7 @@ export function Room() {
     const params = useParams<RoomParams>();
     const roomId = params.id;
     const [newQuestion, setNewQuestion] = useState('');
-    const [questions, setQuestions] = useState<Question[]>([]);
+    const [questions, setQuestions] = useState<QuestionType[]>([]);
     const [title, setTitle] = useState('');
 
     useEffect(() => {
@@ -121,6 +122,19 @@ export function Room() {
                         <Button type="submit" disabled={!user}>Enviar pergunta</Button>
                     </div>
                 </form>
+
+                <div className="question-list">
+                    {questions.map(question => {
+                        return (
+                            <Question
+                                key={question.id} // algorítimo de reconciliação
+                                content={question.content}
+                                author={question.author}
+                            />
+                        );
+                    })}
+                </div>
+
             </main>
         </div>
     );
